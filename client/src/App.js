ad1fcbc162212eeb210d4796bf5c53493bf9ceb9
Vishal Vashishth <vishal112vp@gmail.com>
2026-03-30 18:34:00 +0530

    client

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import { ThemeProvider, styled } from "styled-components";
 import { lightTheme } from "./utils/Themes";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Authentication from "./pages/Authentication";
-import { useState } from "react";
 import { useSelector } from "react-redux";
 import Navbar from "./components/Navbar";
 import Dashboard from "./pages/Dashboard";
